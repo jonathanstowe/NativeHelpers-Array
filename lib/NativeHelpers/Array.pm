9510bc4f22119fe8124afba88ede052d3f5cb08c
Jonathan Stowe <jns+git@gellyfish.co.uk>
2015-07-23 10:01:49 +0100

    Add pod

--- lib/NativeHelpers/Array.pm
+++ lib/NativeHelpers/Array.pm
@@ -4,9 +4,42 @@ use v6;
 
 =head1 NAME
 
+NativeHelpers::Array - helper subroutines for native arrays
+
 =head1 DESCRIPTION
 
+This provides a number of exported subroutines that may be useful for
+working with native L<CArray>s.  The subroutines aren't particularly
+complicated but it saves having to have the same code in multiple modules.
+
 =head1 SUBROUTINES
+
+=head2 sub copy-to-carray
+
+    sub copy-to-carray(@items, Mu $type) returns CArray is export
+
+Copy the array to the new CArray typed as per C<$type>. If the supplied type
+is not a valid native type there will be an exception.  If the elements of the
+array are out of range for the type then the values in the CArray may be 
+truncated.
+
+=head2 sub copy-to-array
+
+    sub copy-to-array(CArray $carray, Int $items) returns Array is export
+
+Copy the supplied CArray of the given length to a new Array.
+
+=head2 sub copy-buf-to-carray
+
+    sub copy-buf-to-carray(Buf $buf) returns CArray[uint8] is export
+
+Copy the elements of the supplied L<Buf> to a L<CArray[uint8]>.
+
+=head2 sub copy-carray-to-buf(
+
+    sub copy-carray-to-buf(CArray $array, Int $no-elems) returns Buf is export
+
+Copy the L<CArray[uint8]> of the specified number of elements to a L<Buf>
 
 =end pod
 
